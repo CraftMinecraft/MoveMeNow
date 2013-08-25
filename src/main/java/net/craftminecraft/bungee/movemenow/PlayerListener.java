2ec2d39bdd611b86a4b7d8ac43fc51deef45218e
roblabla <roblabla@192.168.1.50>
2013-08-25 19:41:45 +0200

    Fix error with a recent bungee change.

--- src/main/java/net/craftminecraft/bungee/movemenow/PlayerListener.java
+++ src/main/java/net/craftminecraft/bungee/movemenow/PlayerListener.java
@@ -1,6 +1,7 @@
 package net.craftminecraft.bungee.movemenow;
 
 import java.util.Iterator;
+import net.md_5.bungee.api.ReconnectHandler;
 
 import net.md_5.bungee.api.config.ServerInfo;
 
@@ -11,15 +12,20 @@ import net.md_5.bungee.api.event.ServerKickEvent;
 import net.md_5.bungee.event.EventHandler;
 
 public class PlayerListener implements Listener {
-	MoveMeNow plugin;
-	public PlayerListener(MoveMeNow plugin) {
-		this.plugin = plugin;
-	}
 
-	@EventHandler
-	public void onServerKickEvent(ServerKickEvent ev) {
+    MoveMeNow plugin;
+
+    public PlayerListener(MoveMeNow plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onServerKickEvent(ServerKickEvent ev) {
         // Protection against NullPointerException
-        ServerInfo kickedFrom = this.plugin.getProxy().getReconnectHandler().getServer(ev.getPlayer());
+        ServerInfo kickedFrom = null;
+        if (this.plugin.getProxy().getReconnectHandler() != null) {
+            kickedFrom = this.plugin.getProxy().getReconnectHandler().getServer(ev.getPlayer());
+        }
         if (ev.getPlayer().getServer() != null) {
             kickedFrom = ev.getPlayer().getServer().getInfo();
         }
@@ -27,36 +33,38 @@ public class PlayerListener implements Listener {
         ServerInfo kickTo = this.plugin.getProxy().getServerInfo(plugin.getConfig().servername);
 
         // Avoid the loop
-        if (kickedFrom.equals(kickTo)) {
+        if (kickedFrom != null && kickedFrom.equals(kickTo)) {
             return;
         }
 
-		Iterator<String> it = this.plugin.getConfig().list.iterator();
-		if (this.plugin.getConfig().mode.equals("whitelist")) {
-			boolean good = false;
-			while (it.hasNext()) {
-				String next = it.next();
-				if (ev.getKickReason().contains(next)) {
-					good = true;
-				}
-			}
-			if (good) {
-				ev.setCancelled(true);
-				ev.setCancelServer(kickTo);
-				if (!plugin.getConfig().movemsg.trim().isEmpty())
-					ev.getPlayer().sendMessage(plugin.getConfig().parsemovemsg(ev.getKickReason()));
-			}
-		} else {
-			while (it.hasNext()) {
-				String next = it.next();
-				if (ev.getKickReason().contains(next)) {
-					return;
-				}
-			}
-			ev.setCancelled(true);
-			ev.setCancelServer(kickTo);
-			if (!plugin.getConfig().movemsg.trim().isEmpty())
-				ev.getPlayer().sendMessage(plugin.getConfig().parsemovemsg(ev.getKickReason()));
-		}
-	}
+        Iterator<String> it = this.plugin.getConfig().list.iterator();
+        if (this.plugin.getConfig().mode.equals("whitelist")) {
+            boolean good = false;
+            while (it.hasNext()) {
+                String next = it.next();
+                if (ev.getKickReason().contains(next)) {
+                    good = true;
+                }
+            }
+            if (good) {
+                ev.setCancelled(true);
+                ev.setCancelServer(kickTo);
+                if (!plugin.getConfig().movemsg.trim().isEmpty()) {
+                    ev.getPlayer().sendMessage(plugin.getConfig().parsemovemsg(ev.getKickReason()));
+                }
+            }
+        } else {
+            while (it.hasNext()) {
+                String next = it.next();
+                if (ev.getKickReason().contains(next)) {
+                    return;
+                }
+            }
+            ev.setCancelled(true);
+            ev.setCancelServer(kickTo);
+            if (!plugin.getConfig().movemsg.trim().isEmpty()) {
+                ev.getPlayer().sendMessage(plugin.getConfig().parsemovemsg(ev.getKickReason()));
+            }
+        }
+    }
 }
